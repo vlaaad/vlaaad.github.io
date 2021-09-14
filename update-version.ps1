9455010f2c3b62804215cc6a0edb0635652b9a4c
vlaaad <v.proczenko@gmail.com>
2021-09-14 15:40:38 +0200

    Free: 1.3.214, Pro: 1.3.241

--- update-version.ps1
+++ update-version.ps1
@@ -2,7 +2,7 @@ $free_version = (curl -s https://clojars.org/api/artifacts/vlaaad/reveal | Conve
 $pro_version = (curl -s https://clojars.org/api/artifacts/dev.vlaaad/reveal-pro | ConvertFrom-Json).latest_release
 
 (Get-Content .\reveal.md) | ForEach-Object {$_ -replace 'vlaaad/reveal {:mvn/version ".+"',"vlaaad/reveal {:mvn/version `"$free_version`""} | Set-Content .\reveal.md
-(Get-Content .\reveal-pro.md) | ForEach-Object {$_ -replace 'dev.vlaaad/reveal-pro {:mvn/version ".+"',"dev.vlaaad/reveal-pro {:mvn/version `"$free_version`""} | Set-Content .\reveal-pro.md
+(Get-Content .\reveal-pro.md) | ForEach-Object {$_ -replace 'dev.vlaaad/reveal-pro {:mvn/version ".+"',"dev.vlaaad/reveal-pro {:mvn/version `"$pro_version`""} | Set-Content .\reveal-pro.md
 
 git add . 
 git commit -am "Free: $free_version, Pro: $pro_version"
