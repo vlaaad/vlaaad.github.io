2a3d5114f91645f438141492030e2ec7c69607fd
vlaaad <v.proczenko@gmail.com>
2022-04-22 14:27:46 +0200

    Free: `1.3.273`, Pro: `1.3.344`

--- update-version.ps1
+++ update-version.ps1
@@ -3,12 +3,14 @@ $pro_version = (curl -s https://clojars.org/api/artifacts/dev.vlaaad/reveal-pro 
 
 Get-ChildItem -Filter *.md | ForEach-Object {
     
-    (Get-Content $_.Name) | ForEach-Object { $_ -replace 'vlaaad/reveal {:mvn/version ".+"', "vlaaad/reveal {:mvn/version `"$free_version`"" } | Set-Content $_.Name
-    (Get-Content $_.Name) | ForEach-Object { $_ -replace '\[vlaaad/reveal ".+"\]', "[vlaaad/reveal `"$free_version`"]" } | Set-Content $_.Name
-    (Get-Content $_.Name) | ForEach-Object { $_ -replace 'dev.vlaaad/reveal-pro {:mvn/version ".+"', "dev.vlaaad/reveal-pro {:mvn/version `"$pro_version`"" } | Set-Content $_.Name
+    (Get-Content $_.Name) 
+      | ForEach-Object { $_ -replace 'vlaaad/reveal {:mvn/version ".+"', "vlaaad/reveal {:mvn/version `"$free_version`"" } 
+      | ForEach-Object { $_ -replace '\[vlaaad/reveal ".+"\]', "[vlaaad/reveal `"$free_version`"]" }
+      | ForEach-Object { $_ -replace 'dev.vlaaad/reveal-pro {:mvn/version ".+"', "dev.vlaaad/reveal-pro {:mvn/version `"$pro_version`"" }
+      | Set-Content $_.Name
 }
 
 
-# git add . 
-# git commit -am "Free: $free_version, Pro: $pro_version"
+git add . 
+git commit -am "Free: ``$free_version``, Pro: ``$pro_version``"
 # git push
